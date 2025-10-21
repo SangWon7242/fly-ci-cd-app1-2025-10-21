3a58938540002c07923543e09d8d71da7e593250
SangWon7242 <gotkdqja@gmail.com>
2025-10-21 20:20:29 +0900

    work

---
+++ src/main/java/com/back/FlyCiCdApp1Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyCiCdApp1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlyCiCdApp1Application.class, args);
+  }
+
+}
